d7840de3688a3c959a837a2298387fe83b5c8fe5
Chidi O <conwunaibe@gmail.com>
2026-04-28 13:16:46 -0400

    Login form js need to define login and html and routes

--- app/models.py
+++ app/models.py
@@ -1,6 +1,17 @@
 from app import db
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms import TextAreaField
+from wtforms.validators import Length
+from wtforms.widgets import NumberInput
 
 class Alert(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	action = db.Column()
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    Email = StringField('Email', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
